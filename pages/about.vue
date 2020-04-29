59f83440628d1ded964c70d55a08b6246c136751
Maxime Barber <maximebarber@gmail.com>
2020-04-29 16:53:49 +0200

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>About page</h1>
+    <p>Vue and Nuxt web app</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'About the app',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Vue and Nuxt web app'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
